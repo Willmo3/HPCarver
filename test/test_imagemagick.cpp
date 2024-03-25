456c4bc43465167bdbff59936bff5a8475ba89a9
morri2wj <morri2wj@dukes.jmu.edu>
2024-03-25 12:44:52 -0400

    Add basic google test

---
+++ test/test_imagemagick.cpp
@@ -0,0 +1,13 @@
+//
+// Created by morri2wj on 3/25/24.
+//
+
+#include <gtest/gtest.h>
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+    // Expect two strings not to be equal.
+    EXPECT_STRNE("hello", "world");
+    // Expect equality.
+    EXPECT_EQ(7 * 6, 42);
+}
