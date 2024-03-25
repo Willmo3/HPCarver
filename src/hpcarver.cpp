57cb39a754e3d28f008c3fdb234a6fe54c49c339
morri2wj <morri2wj@dukes.jmu.edu>
2024-03-25 13:14:41 -0400

    Add basic image magick linking

--- src/hpcarver.cpp
+++ src/hpcarver.cpp
@@ -6,7 +6,12 @@
 
 using namespace Magick;
 
+// For now, only horizontal carving.
+// end goal: specify target dimensions
 int main(int argc, char* argv[]) {
     InitializeMagick(*argv);
     std::cout << "Hello, world!" << std::endl;
 }
+
+// What to do?
+// Driver for c
--- test/test_imagemagick.cpp
+++ test/test_imagemagick.cpp
@@ -3,11 +3,11 @@
 //
 
 #include <gtest/gtest.h>
+#include <Magick++.h>
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-    // Expect two strings not to be equal.
-    EXPECT_STRNE("hello", "world");
-    // Expect equality.
-    EXPECT_EQ(7 * 6, 42);
+using namespace Magick;
+
+TEST(magickloadimage, BasicAssertions) {
+    char *path = "";
+    InitializeMagick(path);
 }
