f4bf0c034221cf4324a54454410c0893df601702
willmo3 <thelordofbouge@gmail.com>
2024-04-07 08:10:49 -0400

    Add horizontal, vertical seam tests

--- test/test_serialcarve.cpp
+++ test/test_serialcarve.cpp
@@ -8,7 +8,7 @@
 
 // Testing helper.
 hpimage::Hpimage test() {
-    return hpimage::Hpimage("3x4.ppm");
+    return hpimage::Hpimage{"3x4.ppm"};
 }
 
 TEST(serialcarve, vertical_carve) {
@@ -20,7 +20,37 @@ TEST(serialcarve, vertical_carve) {
     seam.push_back(2);
 
     serial::remove_vert_seam(image, seam);
+    // Dimensions should be modified.
+    ASSERT_EQ(2, image.cols());
+    ASSERT_EQ(4, image.rows());
+    // Now, check that each seam removal has occured as expected.
+    ASSERT_EQ(153, image.get_pixel(0, 0).blue);
+    ASSERT_EQ(255, image.get_pixel(1, 0).blue);
+    ASSERT_EQ(255, image.get_pixel(1, 1).blue);
 
-    image.write_image("testseam.png");
+    ASSERT_EQ(255, image.get_pixel(1, 2).blue);
+
+    ASSERT_EQ(153, image.get_pixel(1, 3).blue);
+}
+
+TEST(serialcarve, horizontal_carve) {
+    auto image = test();
+    auto seam = std::vector<uint32_t>();
+    seam.push_back(0);
+    seam.push_back(1);
+    seam.push_back(2);
+
+    serial::remove_horiz_seam(image, seam);
+    ASSERT_EQ(3, image.cols());
+    ASSERT_EQ(3, image.rows());
+
+    ASSERT_EQ(153, image.get_pixel(0, 0).green);
+    ASSERT_EQ(203, image.get_pixel(0, 1).green);
+    ASSERT_EQ(255, image.get_pixel(0, 2).green);
+
+    ASSERT_EQ(204, image.get_pixel(1, 1).green);
+    ASSERT_EQ(255, image.get_pixel(1, 2).green);
+
+    ASSERT_EQ(255, image.get_pixel(2, 2).green);
 }
 
