1470bcefeabddc2783bec6c773adf6eb7e9752f2
willmo3 <thelordofbouge@gmail.com>
2024-04-10 16:39:14 -0400

    Remove arg parsing helper and place in main

--- src/hpcarver.cpp
+++ src/hpcarver.cpp
@@ -8,25 +8,12 @@
 // Print usage data
 void usage();
 
+// Indexes for argument parsing
 #define ARG_COUNT 5
 #define SOURCE_INDEX 1
 #define TARGET_INDEX 2
 #define WIDTH_INDEX 3
 #define HEIGHT_INDEX 4
-
-/**
- * Parse arguments for necessary fields. Note that all of these are required!
- *
- * @param argc Number of arguments. Should be 5 -- one extra argument for program name.
- * @param argv Vector of arguments
- * @param source_path Path of source image to convert.
- * @param out_path Path to image to output
- * @param new_width Width of new image. Should be lower than input image -- we currently only support scaling down.
- * @param new_height Height of new image.
- */
-void parse_args(int argc, char *argv[],
-        char *&source_path, char *&out_path, uint32_t &new_width, uint32_t &new_height);
-
 
 // ***** PROGRAM ENTRY POINT ***** //
 
@@ -34,15 +21,17 @@ int main(int argc, char *argv[]) {
     // For now, default to sequential execution
     using policy = RAJA::seq_exec;
 
-    char *source_path = nullptr;
-    char *target_path = nullptr;
-    // Initialized to negative number as param validation.
-    uint32_t new_width = 0;
-    uint32_t new_height = 0;
+    // There should be 5 options, including executable name
+    if (argc != ARG_COUNT) {
+        usage();
+        exit(EXIT_FAILURE);
+    }
 
-    parse_args(argc, argv, source_path, target_path, new_width, new_height);
+    char *source_path = argv[SOURCE_INDEX];
+    char *out_path = argv[TARGET_INDEX];
+    uint32_t new_width = std::stoi(argv[WIDTH_INDEX]);
+    uint32_t new_height = std::stoi(argv[HEIGHT_INDEX]);
 
-    // HPCarver only supports width < three.
     if (new_width < 3) {
         std::cerr << "ERROR: Must specify new image width greater than three!" << std::endl;
         exit(EXIT_FAILURE);
@@ -73,24 +62,7 @@ int main(int argc, char *argv[]) {
     }
 
     // With image dimensions sufficiently changed, write out the target image.
-    image.write_image(target_path);
-}
-
-
-// ***** ARGUMENT MANAGERS ***** //
-
-void parse_args(int argc, char *argv[],
-        char *&source_path, char *&out_path, uint32_t &new_width, uint32_t &new_height) {
-    // There should be "9" options.
-    if (argc != ARG_COUNT) {
-        usage();
-        exit(EXIT_FAILURE);
-    }
-
-    source_path = argv[SOURCE_INDEX];
-    out_path = argv[TARGET_INDEX];
-    new_width = std::stoi(argv[WIDTH_INDEX]);
-    new_height = std::stoi(argv[HEIGHT_INDEX]);
+    image.write_image(out_path);
 }
 
 void usage() {
