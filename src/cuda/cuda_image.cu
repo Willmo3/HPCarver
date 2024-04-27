2e229452d94116aebf6a2a54df6c814ddaf7cae7
morri2wj <morri2wj@dukes.jmu.edu>
2024-04-27 17:12:00 -0400

    Add cuda image allocate function

--- src/cuda/cuda_image.cu
+++ src/cuda/cuda_image.cu
@@ -1,3 +1,13 @@
 #include "cuda_image.h"
 
 using namespace hpc_cuda;
+
+hpimage::pixel *CudaImage::alloc(int size) {
+    hpimage::pixel *pixel;
+    cudaMallocManaged(&pixel, size);
+    return pixel;
+}
+
+CudaImage::~CudaImage() {
+    cudaFree(pixels);
+}
--- src/cuda/cuda_image.h
+++ src/cuda/cuda_image.h
@@ -1,32 +1,25 @@
+#ifndef HPCARVER_CUDA_IMAGE_H
+#define HPCARVER_CUDA_IMAGE_H
+
 /**
  * CUDA image. Memory allocated using CUDA functions.
  * This is a variant of the HPImage library.
  *
  * Author: Will Morris
  */
-#ifndef HPCARVER_CUDA_IMAGE_H
-#define HPCARVER_CUDA_IMAGE_H
 
 #include "../../HPImage/hpimage.h"
 
 namespace hpc_cuda {
 
 class CudaImage: hpimage::Hpimage {
-public:
-    /**
-     * CudaEnergy constructor.
-     * This initializes a block of memory for an energy matrix using cuda
-     * @param cols Number of columns to initialize energy block with
-     * @param rows Number of rows to initialize energy block with
-     */
-    CudaImage(uint32_t cols, uint32_t rows);
 
-    /**
-     * CudaEnergy destructor.
-     * Will free cuda shared-allocated resources (i.e. the energy block).
-     */
+protected:
+    hpimage::pixel *alloc(uint32_t size) override;
+public:
     ~CudaImage();
 };
+
 }
 
 #endif //HPCARVER_CUDA_IMAGE_H
